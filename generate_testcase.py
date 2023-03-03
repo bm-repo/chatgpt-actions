f6ae652aa19f986875e4e79322f21ac5bd13c8b0
allabakashb <allabakash0@gmail.com>
2023-03-03 22:16:23 +0530

    Update generate_testcase.py

--- generate_testcase.py
+++ generate_testcase.py
@@ -49,9 +49,11 @@ def generate_testcases():
                 content = repo.get_contents(filename, ref=commit.sha).decoded_content
                 
                 testing_framework = 'pytest'
+                function_type = 'function'
                 
                 if '.java' in filename:
                     testing_framework = 'JUnit'
+                    function_type = 'method'
                 elif '.tsx' in filename or '.js' in filename or '.jsx' in filename or '.ts' in filename:
                     testing_framework = 'Jest'
  
@@ -59,7 +61,7 @@ def generate_testcases():
                 response = openai.Completion.create(
                     engine=openai_engine,
                     prompt=(
-                        f"Generate unit tests for following code using {testing_framework}:\n{content}"),
+                        f"Generate unit tests for following {function_type} using {testing_framework}:\n{content}"),
                     temperature=openai_temperature,
                     max_tokens=openai_max_tokens,
                     best_of=openai_best_of
