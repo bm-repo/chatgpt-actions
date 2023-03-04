49a26cd7fc9600cc37c37836378c04a736c7cc14
allabakashb <allabakash0@gmail.com>
2023-03-04 17:09:19 +0530

    Update generate_review.py

--- generate_review.py
+++ generate_review.py
@@ -62,9 +62,9 @@ def generate_review():
                 # Adding a comment to the pull request with ChatGPT's response
                 review_comment = '\n\n'.join( [ x['text'] for x in response['choices'] ] )
 
-                formatted_comment = '<img src="https://raw.githubusercontent.com/allabakashb/SampleJSON/main/logo.png" width="100px"><div>You can improve the code quality by following suggestions for <b>{file.filename}</b>:{review_comment}</div>\n> [Good suggestion✅]()  \n--\n> [Incorrect/bad suggestion❌]()'
-                pull_request.create_issue_comment(f'${formatted_comment}')
-                print(f'${formatted_comment}')
+                formatted_comment = f'<img src="https://raw.githubusercontent.com/allabakashb/SampleJSON/main/logo.png" width="100px"><div>You can improve the code quality by following suggestions for <b>{file.filename}</b>:{review_comment}</div>\n> [Good suggestion✅]()  \n--\n> [Incorrect/bad suggestion❌]()'
+                pull_request.create_issue_comment(formatted_comment)
+                print(formatted_comment)
                 time.sleep(30)
     except Exception as ex:
         print('exception generated', ex.args)
